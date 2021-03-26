3d17090545d15e0584cc425441593cb9b716bed4
coderish <32035238+coderish@users.noreply.github.com>
2021-03-26 23:22:49 +0530

    code commit

---
+++ src/test/java/com/example/quartzdemo/QuartzDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quartzdemo;
+
+import org.junit.jupiter.api.Test;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class QuartzDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
